a558a2a48cac8da91e4dd3f750160343dfc0a9a9
Ezequiel Leandro Jr. <55987716+Ezequiel-leandro-jr@users.noreply.github.com>
2025-08-03 18:27:06 -0300

    Update Teste.java

--- src/teste/Teste.java
+++ src/teste/Teste.java
@@ -2,104 +2,116 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package teste;
-
-import excecoes.LoginIncorretoException;
-import java.util.ArrayList;
-import java.util.Scanner;
+package utils;
+import dados.*;
 import negocio.Consulta;
 import negocio.Medico;
 import negocio.Paciente;
+import negocio.Pessoa;
 import negocio.Usuario;
-
 /**
  *
- * @author ezequiel
+ * @author lucas
  */
 public class Teste {
-    
-    public static void main(String[] args) throws LoginIncorretoException{
-        Scanner input = new Scanner(System.in);
-        int op = 1;
+    public static void main(String[] args) {
         
-        //TESTE DE LOGIN DO ADMINISTRADOR
-        Usuario user = new Usuario("admin", "123");
-        while(op == 1){
-            System.out.println("AGBA CLÍNICA MÉDICA\n--------------\n");
-            System.out.println("LOGIN: ");
-            String login = input.nextLine();
-            System.out.println("SENHA: ");
-            String senha = input.nextLine();
- 
-            user.Login(login, senha);    
-        }
-        
-        //TESTE CRUD MÉDICO
-        //cadastrar
-        String nome = "Robson";
-        String sobrenome = "Clebson";
-        int idade = 55;
-        String data_nascimento = "11/11/1969" ;
-        String genero = "Masculino";
-        String telefone = "(81)99999-9999";
-        String email = "doutordogera@email.com";
-        String crm = "4543-66";
-        String especialidade = "Gastroenterologia";
-        
-        Medico novoMedico = new Medico(nome, sobrenome, data_nascimento, genero, crm, especialidade, email, telefone);
-        
-        novoMedico.toString();
-        
-        ArrayList<Medico> medicos = new ArrayList<>();
-        medicos.add(novoMedico);
-        
-        novoMedico = new Medico("Carlo", "Ancelotti", "11/03/1992", "Masculino", "1234-56", "Cardiologia", "carlao@email.com", "(81)9999-9765");
-        medicos.add(novoMedico);
+        // testes das entidades
+        System.out.println("#### Teste das Entidades ####");
+        Usuario usuario = new Usuario("usuario_teste", "123");
+        System.out.println(usuario);
         
         
-        
-        //TESTE CRUD PACIENTE
-        //cadastrar
-        nome = "Edgleibson";
-        sobrenome = "Hudson";
-        idade = 25;
-        data_nascimento = "11/05/2000";
-        genero = "Masculino";
-        telefone = "(81)9876-9999";
-        email = "edhud@email.com";
-        String cpf = "027.467.153-65";
-        Paciente novoRegistro = new Paciente(nome, sobrenome, data_nascimento, genero, telefone, email, cpf);
-        
-        novoRegistro.toString();
-        
-        ArrayList<Paciente> pacientes = new ArrayList<>();
-        pacientes.add(novoRegistro);
-        
-        novoRegistro = new Paciente("Esmirgulina", "Virgulina", "11/09/2001", "Feminina", "(81)9432-8765", "esmirgulinda@email.com", "012.536.784-77");
-        pacientes.add(novoRegistro);
+        Pessoa pessoa = new Pessoa("Pessoa", " de Teste", "14/12/2000", "Masculino", "(81)98888-7458", "pessoa.teste@email.com");
+        System.out.println(pessoa);
         
         
+        Paciente paciente = new Paciente("Paciente", " de Teste", "14/12/2000", "Masculino", "(81)98888-7458", "pessoa.teste@email.com", "111.156.321-87");
+        System.out.println(paciente);
+        Paciente paciente_2 = new Paciente("Pessoa", " de Teste Dois", "14/12/2000", "Masculino", "(81)98888-7458", "pessoa.teste@email.com", "111.156.321-87");
+
         
-        //TESTE CRUD CONSULTA
-        //cadastrar
-        String id_consulta = "001";
-        String data_consulta = "17/07/2025";
-        String id_paciente = "027.467.153-65";
-        String id_medico = "4543-66";
-        String consultorio = "1";
-        String hora_consulta = "08:30";
-        Consulta nova = new Consulta(id_consulta, data_consulta, id_paciente, id_medico, consultorio, hora_consulta);
-        
-        nova.toString();
-        
-        ArrayList<Consulta> consultas = new ArrayList<>();
-        consultas.add(nova);
-        
-        nova = new Consulta("002", "11/05/2025", "034.714.911-55", "1234-56", "2", "14:30");
-        consultas.add(nova);
+        Medico medico = new Medico(2, "Médico", " de Teste", "14/10/1990", "Feminino", "123PE", "Pediatria", "medico.teste@email.com", "(81)98888-7458");
+        Medico medico_2 = new Medico(2, "Médico", " de Teste Dois", "14/10/1990", "Masculino", "321PE", "Geriatra", "medico.teste@email.com", "(81)98888-7458");
+        System.out.println(medico);
         
         
+        Consulta consulta = new Consulta(8, "pdtr-01", "03/08/2025", "1", "2", "4", "08:15");
+        Consulta consulta_2 = new Consulta(9, "pdtr-03", "03/08/2025", "1", "2", "4", "10:15");
+        Consulta consulta_3 = new Consulta(10, "pdtr-005", "03/08/2025", "1", "2", "4", "11:15");
+        System.out.println(consulta);
+        System.out.println("####### #######");
+
+        // testes repositório de médicos
+        // adicionar
+        System.out.println("#### Teste Repositórios de Médicos ####");
+        RepositorioTesteMedico repositorio_medicos = new RepositorioTesteMedico();
+        repositorio_medicos.adicionar(medico);
+        
+        // atualizar
+        Medico medico_atualizado = new Medico(2, "Médico", " de Teste Atualizado", "11/10/1988", "Feminino", "124PE", "Pediatria", "medico.teste@email.com", "(81)98888-7458");
+        repositorio_medicos.atualizar(medico, medico_atualizado);
+        
+        // buscar
+        System.out.println("#### Resultado busca de médicos: ####" + repositorio_medicos.buscar(medico_atualizado));
+        System.out.println("####### #######");
+        
+        // deletar
+        repositorio_medicos.remover(medico_2);
+        
+        // listar
+        System.out.println("#### Listagem de Médicos: ####");
+        repositorio_medicos.listar();
+        System.out.println("####### #######");
+
         
         
+        // testes repositório de pacientes
+        // adicionar
+        System.out.println("#### Teste Repositório de Pacientes ####");
+        RepositorioTestePaciente repositorio_pacientes = new RepositorioTestePaciente();
+        repositorio_pacientes.adicionar(paciente);
+        repositorio_pacientes.adicionar(paciente_2);
+        
+        // atualizar
+        Paciente paciente_atualizado = new Paciente("Paciente", "de Teste Atualizada", "14/12/2000", "Masculino", "(81)98888-7458", "pessoa.teste@email.com", "111.156.321-87");
+        repositorio_pacientes.atualizar(paciente, paciente_atualizado);
+        
+        // buscar
+        System.out.println("#### Resultado busca de consultas: ####" + repositorio_pacientes.buscar(paciente_atualizado));
+        System.out.println("####### #######");
+        
+        // deletar
+        repositorio_pacientes.remover(paciente_2);
+        
+        // listar
+        System.out.println("#### Listagem de Pacientes: ####");
+        repositorio_pacientes.listar();
+        System.out.println("####### #######");
+
+        
+        
+        // testes repositório de consultas
+        // adicionar
+        System.out.println("#### Teste Repositório de Consultas ####");
+        RepositorioTesteConsulta repositorio_consultas = new RepositorioTesteConsulta();
+        repositorio_consultas.adicionar(consulta);
+        repositorio_consultas.adicionar(consulta_2);
+        repositorio_consultas.adicionar(consulta_3);
+        
+        // atualizar
+        Consulta consulta_atualizada = new Consulta(8, "pdtr-02", "03/08/2025", "1", "2", "4", "08:30");
+        repositorio_consultas.atualizar(consulta, consulta_atualizada);
+        
+        // buscar
+        System.out.println("#### Resultado busca de consultas: ####" + repositorio_consultas.buscar(consulta));
+        
+        // deletar
+        repositorio_consultas.remover(consulta_2);
+        
+        // listar
+        System.out.println("#### Listagem de consultas: ####");
+        repositorio_consultas.listar();
+        System.out.println("####### #######");
     }
 }
